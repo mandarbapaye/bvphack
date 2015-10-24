8e9704bfbc1e5c76410a06ad057dc86cfb8c6c95
Mandar Bapaye <mandarbapaye@gmail.com>
2015-10-24 02:49:24 -0700

    View changes

--- app/src/main/java/bvp/bvphackthon/adapters/PostsListAdapter.java
+++ app/src/main/java/bvp/bvphackthon/adapters/PostsListAdapter.java
@@ -10,6 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 
 import java.util.List;
@@ -20,59 +22,144 @@ import bvp.bvphackthon.R;
 import bvp.bvphackthon.models.Post;
 import bvp.bvphackthon.views.ParseImageView;
 
+//public class PostsListAdapter extends ArrayAdapter<Post> {
+//
+//    static class ViewHolder {
+//        @InjectView(R.id.ivFoodImageList)
+//        ParseImageView ivFoodImageList;
+//
+//        @InjectView(R.id.tvMinsList)
+//        TextView tvMinsList;
+//
+//        @InjectView(R.id.tvMilesList)
+//        TextView tvMilesList;
+//
+//        @InjectView(R.id.tvMealsList)
+//        TextView tvMealsList;
+//
+//        @InjectView(R.id.tvPostDescription)
+//        TextView tvPostDescription;
+//
+//        public ViewHolder(View view) {
+//            ButterKnife.inject(this, view);
+//        }
+//    }
+//
+//    public PostsListAdapter(Context context, List<Post> posts) {
+//        super(context, 0, posts);
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        Post post = getItem(position);
+//
+//        final ViewHolder viewHolder;
+//        if (convertView != null) {
+//            viewHolder = (ViewHolder) convertView.getTag();
+//        } else {
+//            LayoutInflater inflater = LayoutInflater.from(getContext());
+//            convertView = inflater.inflate(R.layout.post_list_item, parent, false);
+//            viewHolder = new ViewHolder(convertView);
+//            convertView.setTag(viewHolder);
+//        }
+//
+//        viewHolder.tvMealsList.setText(String.valueOf(post.getNumberOfFeeders()));
+//        viewHolder.tvMinsList.setText("20");
+//        viewHolder.tvMilesList.setText(String.valueOf(Math.random() * 10));
+//        viewHolder.tvPostDescription.setText(post.getDescription());
+//
+//        ParseFile photoFile = post.getPhoto();
+//        if (photoFile != null) {
+//            viewHolder.ivFoodImageList.loadParseFileImageInBackground(photoFile);
+//        }
+//
+//        return convertView;
+//    }
+//
+//}
+
 public class PostsListAdapter extends ArrayAdapter<Post> {
 
-    static class ViewHolder {
-        @InjectView(R.id.ivFoodImageList)
+    private static class ViewHolder {
         ParseImageView ivFoodImageList;
-
-        @InjectView(R.id.tvMinsList)
         TextView tvMinsList;
-
-        @InjectView(R.id.tvMilesList)
         TextView tvMilesList;
-
-        @InjectView(R.id.tvMealsList)
         TextView tvMealsList;
-
-        @InjectView(R.id.tvPostDescription)
         TextView tvPostDescription;
-
-        public ViewHolder(View view) {
-            ButterKnife.inject(this, view);
-        }
+        int position;
     }
 
     public PostsListAdapter(Context context, List<Post> posts) {
         super(context, 0, posts);
     }
 
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        Student student = getItem(position);
+//
+//        final ViewHolder viewHolder;
+//        if (convertView != null) {
+//            viewHolder = (ViewHolder) convertView.getTag();
+//        } else {
+//            viewHolder = new ViewHolder();
+//            LayoutInflater inflater = LayoutInflater.from(getContext());
+//            convertView = inflater.inflate(R.layout.student_list_item, parent, false);
+//            viewHolder.rpivListStudentImage = (RoundedParseImageView) convertView.findViewById(R.id.rpivListStudentImage);
+//            viewHolder.tvListStudentName = (TextView) convertView.findViewById(R.id.tvListStudentName);
+//            convertView.setTag(viewHolder);
+//        }
+//
+//        String currentItemNameInConvertView = viewHolder.tvListStudentName.getText().toString();
+//        if (!currentItemNameInConvertView.equalsIgnoreCase(student.getName())) {
+//            viewHolder.tvListStudentName.setText(student.getName());
+//            Log.d("test", "~~~~~ Name: " + student.getName());
+//            viewHolder.rpivListStudentImage.setImageResource(R.drawable.ic_placeholder);
+//            viewHolder.rpivListStudentImage.loadParseFileImageInBackground(student.getProfileImage());
+//        }
+//
+//        return convertView;
+//    }
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         Post post = getItem(position);
 
         final ViewHolder viewHolder;
         if (convertView != null) {
             viewHolder = (ViewHolder) convertView.getTag();
         } else {
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.post_list_item, parent, false);
-            viewHolder = new ViewHolder(convertView);
+            viewHolder.ivFoodImageList = (ParseImageView) convertView.findViewById(R.id.ivFoodImageList);
+            viewHolder.tvMealsList = (TextView) convertView.findViewById(R.id.tvMealsList);
+            viewHolder.tvMinsList = (TextView) convertView.findViewById(R.id.tvMinsList);
+            viewHolder.tvMilesList = (TextView) convertView.findViewById(R.id.tvMilesList);
+            viewHolder.tvPostDescription = (TextView) convertView.findViewById(R.id.tvPostDescription);
             convertView.setTag(viewHolder);
         }
 
+        viewHolder.position = position;
         viewHolder.tvMealsList.setText(String.valueOf(post.getNumberOfFeeders()));
         viewHolder.tvMinsList.setText("20");
         viewHolder.tvMilesList.setText(String.valueOf(Math.random() * 10));
         viewHolder.tvPostDescription.setText(post.getDescription());
 
-        ParseFile photoFile = post.getPhoto();
-        if (photoFile != null) {
-            viewHolder.ivFoodImageList.loadParseFileImageInBackground(photoFile);
-        }
+        viewHolder.ivFoodImageList.loadParseFileImageInBackground(post.getPhoto(), new GetDataCallback() {
+            public void done(byte[] data, ParseException e) {
+                if (e == null) {
+                    if (viewHolder.position == position) {
+                        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        viewHolder.ivFoodImageList.setImageBitmap(bmp);
+                        viewHolder.ivFoodImageList.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
 
         return convertView;
     }
+
 
 }
 
